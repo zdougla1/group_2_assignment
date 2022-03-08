0e5b987761c5fe7dad4970efdb4a7064075117ae
mtoale <mtoale@vols.utk.edu>
2022-03-08 15:20:17 -0500

    data + server and ui

---
+++ global.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(fpp3)
+library(readr)
+
+# Read zipped data
+stocks <- read_csv("nyse_stocks.csv.zip")
+
+# Convert to `tsibble()`
+stocks$date <- as.Date(stocks$date)
+stocks <- tsibble(stocks, index = date, key = symbol)
+
+# 1 stock
+selected_stock <- "AAPL"
+
+stocks %>%
+  filter(symbol == selected_stock) %>%
+  autoplot(open) +
+  labs(title = selected_stock)
+
+# Multiple stocks
+selected_stocks <- c("GOOG", "AAPL")
+
+stocks %>%
+  filter(symbol %in% selected_stocks) %>%
+  autoplot(open)
